2949ecc6b8f469246ce75937709d64504a130ee6
Patrick Kinimond Hassø <89964910+Kinimond@users.noreply.github.com>
2021-09-13 14:58:58 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 5;
+        if (x < 10 && x > 0) {
+            System.out.println("positive single digit number.");
+            }
+        }
+
+    }
